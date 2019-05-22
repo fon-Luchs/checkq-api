10009401ec27d4ab9bac4b1a3be7c54b1221ba07
Vladislav <lvo123ua@gmail.com>
2019-05-22 19:56:58 +0300

    - tiny tiny changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource 'api/subjects',
-      headers: [ 'Content-type' => 'application/json', 'Accept' => 'application/json' ],
+    resource '*',
+      headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
